09370675fc65a5f8f666d0df7cdb4f6bb301b5a7
Kieran Ricardo <kieran.ricardo@anu.edu.au>
2023-10-30 17:03:56 +1100

    bugfix: import rhoi in calculate_ki_from_Tin

--- cicecore/drivers/nuopc/cmeps/ice_import_export.F90
+++ cicecore/drivers/nuopc/cmeps/ice_import_export.F90
@@ -2052,7 +2052,7 @@ contains
   function calculate_ki_from_Tin (Tink, salink) &
    result(ki)
 
-   use icepack_parameters, only: kice, conduct
+   use icepack_parameters, only: kice, conduct, rhoi
    ! use icepack_therm_bl99, only: kimin, betak
    !
    ! !USES:
